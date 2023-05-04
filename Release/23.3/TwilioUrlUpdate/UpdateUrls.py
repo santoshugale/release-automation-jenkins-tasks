9b18ef123b5f9444f762b042393f25352308b8c3
Santosh <39987598+santoshugale@users.noreply.github.com>
2023-05-04 18:39:49 +0530

    Update UpdateUrls.py

--- Release/23.3/TwilioUrlUpdate/UpdateUrls.py
+++ Release/23.3/TwilioUrlUpdate/UpdateUrls.py
@@ -1,1 +1,8 @@
-
+import csv
+with open("ClientList.csv") as clientList:
+  reader_obj = csv.reader(clientList)
+  for row in reader_obj:
+    print("row - ", row)
+    print("row[0] - ", row[0])
+    print("row[1] - ", row[1])
+    print("row[2] - ", row[2])
