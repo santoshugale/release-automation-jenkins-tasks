fdc6810ab06da404e0c3ac859c7fbaeb043d0198
Santosh <39987598+santoshugale@users.noreply.github.com>
2023-07-13 18:38:58 +0530

    Create DeleteRMQQueues.py

---
+++ Release/23.3.0/RMQQueuesDelete/DeleteRMQQueues.py
@@ -0,0 +1,39 @@
+import sys
+import pika
+import subprocess
+import shlex
+import csv
+
+def run(cmd):
+    return subprocess.check_output(shlex.split(cmd))
+
+def get_queues():
+    with open("queue-list.csv") as queueList:
+        reader_obj = csv.reader(queueList)
+        for row in reader_obj:
+            yield row[0]
+    
+def delete_queues():
+    credentials = pika.PlainCredentials('guest', 'guest')
+    pika.ConnectionParameters('localhost', 15672, "/", credentials)
+    connection = pika.BlockingConnection() 
+    
+    for queue in get_queues():
+        try:
+            channel = connection.channel()
+            print("Purging queue:- ", queue)
+            channel.queue_purge(queue=queue)
+            channel.queue_delete(queue=queue)
+        except Exception as ex:
+            print('unable to delete queue:- ', queue)
+            print('ex-: ', ex)
+
+    connection.close()
+
+def main():
+    try:
+       delete_queues()
+    except Exception as ex:
+        print(ex)
+
+main()
