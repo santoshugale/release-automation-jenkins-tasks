0197c3078a477e3985af13278f868eb3ac184754
Santosh <39987598+santoshugale@users.noreply.github.com>
2023-07-14 16:35:37 +0530

    Create testfile.py

---
+++ Release/23.3.0/RMQQueuesDelete/testfile.py
@@ -0,0 +1,23 @@
+import pika
+import csv
+ 
+print("Queues Deletion Started")
+print("before making connecion")
+credentials = pika.PlainCredentials('guest', 'guest')
+parameters = pika.ConnectionParameters('20.85.124.141', 5672, "/", credentials)
+connection = pika.BlockingConnection(parameters)
+print("after making connecion")
+with open("QueuesList.csv") as queueList:
+    reader_obj = csv.reader(queueList)
+    for row in reader_obj:
+        try:
+            channel = connection.channel()
+            print("Purging queue:- ", row[0])
+            channel.queue_purge(queue=row[0])
+            channel.queue_delete(queue=row[0])
+        except Exception as ex:
+            print('unable to delete queue:- ', row[0])
+            print('ex-: ', ex)        
+connection.close()
+
+print("Queues Deletion Completed")
