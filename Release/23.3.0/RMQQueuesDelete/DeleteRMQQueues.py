f01d5279067637b29e8d998cdab01e230bb29849
Santosh <39987598+santoshugale@users.noreply.github.com>
2023-07-14 19:31:22 +0530

    Update DeleteRMQQueues.py

--- Release/23.3.0/RMQQueuesDelete/DeleteRMQQueues.py
+++ Release/23.3.0/RMQQueuesDelete/DeleteRMQQueues.py
@@ -1,41 +1,21 @@
-import sys
 import pika
-import subprocess
-import shlex
 import csv
-
-def run(cmd):
-    return subprocess.check_output(shlex.split(cmd))
-
-def get_queues():
-    with open("QueuesList.csv") as queueList:
-        reader_obj = csv.reader(queueList)
-        for row in reader_obj:
-            yield row[0]
-    
-def delete_queues():
-    credentials = pika.PlainCredentials('guest', 'guest')
-    pika.ConnectionParameters('localhost', 15672, "/", credentials)
-    connection = pika.BlockingConnection() 
-    
-    for queue in get_queues():
+ 
+print("Queues Deletion Started")
+credentials = pika.PlainCredentials('guest', 'guest')
+parameters = pika.ConnectionParameters('20.85.124.141', 5672, "/", credentials)
+connection = pika.BlockingConnection(parameters)
+with open("QueuesList.csv") as queueList:
+    reader_obj = csv.reader(queueList)
+    for row in reader_obj:
         try:
             channel = connection.channel()
-            print("Purging queue:- ", queue)
-            channel.queue_purge(queue=queue)
-            channel.queue_delete(queue=queue)
+            print("Purging queue:- ", row[0])
+            channel.queue_purge(queue=row[0])
+            channel.queue_delete(queue=row[0])
         except Exception as ex:
-            print('unable to delete queue:- ', queue)
-            print('ex-: ', ex)
+            print('unable to delete queue:- ', row[0])
+            print('ex-: ', ex)        
+connection.close()
 
-    connection.close()
-
-def main():
-    try:
-       delete_queues()
-    except Exception as ex:
-        print(ex)
-
-print("Queues Deletion Started")
-main()
 print("Queues Deletion Completed")
